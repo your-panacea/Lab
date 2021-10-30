f7086fa87b7dacd51396eb393289de26fbf18cd4
your-panacea <lotheb@mail.ru>
2021-10-30 14:35:00 +0300

    Revert "add SecondsInMinute"
    This reverts commit e7f19fb2a1940196c61fe3bcd8a2af3fb8cd17e8.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,7 @@ bool IsLeapYear(int year)
 	return((year % 4 == 0) && (year % 100 != 0) || ((year % 100 == 0) && (year % 400 == 0)));
 }
 
-int SecondsInMinute()
-{
-	return 60;
-}
+
 int main(){
 	
 }
