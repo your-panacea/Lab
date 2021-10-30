cc5afceb7d6aef0ea90e72597914f0a01a2ce0ee
your-panacea <lotheb@mail.ru>
2021-10-30 14:42:13 +0300

    LaterInYear add

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@ bool IsLeapYear(int year)
 	return((year % 4 == 0) && (year % 100 != 0) || ((year % 100 == 0) && (year % 400 == 0)));
 }
 
-
+void LaterInYear(int d1, int m1, int d2, int m2)
+{
+	if (12 - m1 < 12 - m2)
+		cout << d1 << " " << m1;
+	else if (12 - m1 == 12 - m2)
+	{
+		if (d1 > d2)
+			cout << d1 << " " << m1;
+		else cout << d2 << " " << m2;
+	}
+	else 
+		cout << d2 << " " << m2;
+}
 int main(){
 	
 }
