fa338bc8a3f5308dade7fa3c61690bf9b16bdcce
your-panacea <lotheb@mail.ru>
2021-10-30 14:32:07 +0300

    make IsLeapYear

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-int main(){
+#include <iostream>
+using namespace std;
 
+bool IsLeapYear(int year)
+{
+	return((year % 4 == 0) && (year % 100 != 0) || ((year % 100 == 0) && (year % 400 == 0)));
+}
+
+
+int main(){
+	
 }
