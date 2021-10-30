e7f19fb2a1940196c61fe3bcd8a2af3fb8cd17e8
your-panacea <lotheb@mail.ru>
2021-10-30 14:34:03 +0300

    add SecondsInMinute

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ bool IsLeapYear(int year)
 	return((year % 4 == 0) && (year % 100 != 0) || ((year % 100 == 0) && (year % 400 == 0)));
 }
 
-
+int SecondsInMinute()
+{
+	return 60;
+}
 int main(){
 	
 }
